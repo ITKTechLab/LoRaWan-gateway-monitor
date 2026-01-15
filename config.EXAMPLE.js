9a64a2dadbb31f0f61116eeda66cc5ede5793414
Kristian Risager Larsen <github@kezze.dk>
2026-01-15 12:56:30 +0100

    The config file is accessible through any browser, so do not put it on a public web server.

--- config.EXAMPLE.js
+++ config.EXAMPLE.js
@@ -1,3 +1,5 @@
+// This file is accessible through any browser, so do not put it on a public web server.
+
 var config = {
   apiKey : 'API-KEY-HERE',
   url_gateways : 'OS2IoT-BACKEND-URL-HERE/api/v1/chirpstack/gateway?organizationId=ORGANISATION-ID-HERE',
